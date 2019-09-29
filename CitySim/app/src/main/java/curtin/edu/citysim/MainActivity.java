7c386f40ce0991cd60e878983061072fc2e6b47e
julian-heng <julianhengwl@gmail.com>
2019-09-29 20:30:18 +0800

    Add Files

---
+++ CitySim/app/src/main/java/curtin/edu/citysim/MainActivity.java
@@ -0,0 +1,16 @@
+package curtin.edu.citysim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
